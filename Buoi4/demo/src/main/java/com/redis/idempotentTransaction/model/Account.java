81a520f796b5388318c32b19fbc2989e06e86ffe
n0aHVN <superherodung123@gmail.com>
2025-11-10 14:28:31 +0700

    Update Prometheus and Grafana

--- Buoi4/demo/src/main/java/com/redis/idempotentTransaction/model/Account.java
+++ Buoi4/demo/src/main/java/com/redis/idempotentTransaction/model/Account.java
@@ -7,12 +7,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "account")
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
